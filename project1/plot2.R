e7f34d55e2b1b86e85a7f039188e6320c7d3e2a3
Kumar Chandrakant <kchandrakant@sapient.com>
2015-10-21 16:55:56 +0530

    Adding scripts and plots for first course project of course exploratory data science at coursera.

---
+++ project1/plot2.R
@@ -0,0 +1,17 @@
+plot2 <- function() {
+	#Load the sqldf package to selectively load the data
+	library(sqldf)
+  
+	#Read the data assuming the datafile is located in the present working directory in unzipped form
+	data <- read.csv.sql("household_power_consumption.txt", sql="select * from file where Date='1/2/2007' OR Date='2/2/2007'", header=TRUE, sep=";")
+  
+	#Change the class of date and time columns from factor to Date and Posixt
+	data$Time <- paste(data$Date, data$Time)
+	data$Time <- strptime(data$Time, "%d/%m/%Y %H:%M:%S")
+	data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+	#Open the graphics device and plot the required data
+	png('plot2.png')
+	with(set, plot(set$Time, set$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
+	dev.off()
+}
---
+++ project1/plot3.R
@@ -0,0 +1,20 @@
+plot3 <- function() {
+	#Load the sqldf package to selectively load the data
+	library(sqldf)
+  
+	#Read the data assuming the datafile is located in the present working directory in unzipped form
+	data <- read.csv.sql("household_power_consumption.txt", sql="select * from file where Date='1/2/2007' OR Date='2/2/2007'", header=TRUE, sep=";")
+  
+	#Change the class of date and time columns from factor to Date and Posixt
+	data$Time <- paste(data$Date, data$Time)
+	data$Time <- strptime(data$Time, "%d/%m/%Y %H:%M:%S")
+	data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+	#Open the graphics device and plot the required data
+	png('plot3.png')
+	with(set, plot(set$Time, set$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
+	with(set, points(set$Time, set$Sub_metering_2, type="l", col="red"))
+	with(set, points(set$Time, set$Sub_metering_3, type="l", col="blue"))
+	legend("topright", lty=1, cex=0.8, col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+	dev.off()
+}
